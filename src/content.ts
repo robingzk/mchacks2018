cc39b7d0dac0a0587051a0d2c4fac29879f26452
rgzk <robin.guzniczak@gmail.com>
2018-02-03 14:34:58 -0500

    update

--- src/content.ts
+++ src/content.ts
@@ -1,8 +1,9 @@
 //
-// launcher
-// |-- input
-// |-- container
-//     |-- a cmd
+// div launcher
+// |-- input input
+// |-- div container
+//     |-- a cmd1
+//     |-- a cmd2
 
 const commands = ['New tab', 'Bookmark this page', 'Exit', 'Open bookmark...', 'New window']
 const commandIndex = 0
@@ -19,21 +20,28 @@ function createElem(tag: string, style: object) {
 
 const launcher = createElem('div', {
 	width: '400px',
-	backgroundColor: 'blue',
+	backgroundColor: '#39264F',
 	position: 'fixed',
 	top: '0',
 	left: '50%',
 	transform: 'translate(-50%, 0)',
-	padding: '5px'
+	padding: '5px',
+	opacity: '0.95',
+	boxShadow: '0 0 5px 8px rgba(0,0,0,0.2)',
+	borderRadius: '5px',
+	zIndex: '100000'
 })
 document.body.appendChild(launcher)
 
 const input = createElem('input', {
 	placeholder: 'Enter a command',
 	width: '100%',
-	height: '40px',
+	height: '48px',
 	border: '0',
-	fontSize: '18px'
+	fontSize: '20px',
+	marginLeft: '10px',
+	backgroundColor: 'transparent',
+	color: '#eeeeee'
 })
 input.addEventListener('blur', closeLauncher)
 launcher.appendChild(input)
@@ -46,13 +54,21 @@ launcher.appendChild(container)
 generateCommands(commands)
 
 function generateCommands(cmds: string[]) {
+	let i = 0;
 	for (let cmd of cmds) {
-		const cmdElem = createElem('a', {
+		let style = {
 			display: 'block',
-			padding: '10px'
-		})
+			padding: '15px',
+			color: 'white'
+		}
+		if (i === commandIndex) {
+			style['backgroundColor'] = '#614F75'
+			style['color'] = '#fff'
+		}
+		const cmdElem = createElem('a', style)
 		cmdElem.innerText = cmd
 		container.appendChild(cmdElem)
+		i += 1
 	}
 }
 
